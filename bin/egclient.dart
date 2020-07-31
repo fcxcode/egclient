8f4b53400287eaa182ce3e41e8d60f4608743ec0
Fcx Code <fcx.code@gmail.com>
2020-07-31 19:58:49 +0800

    first commit

---
+++ bin/egclient.dart
@@ -0,0 +1,5 @@
+import 'package:egclient/egclient.dart' as egclient;
+
+void main(List<String> arguments) {
+  print('Hello world: ${egclient.calculate()}!');
+}
---
+++ test/egclient_test.dart
@@ -0,0 +1,8 @@
+import 'package:egclient/egclient.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
